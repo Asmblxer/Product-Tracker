bab17db50cd3b6c72dd769fbd9411e93779d7694
Asem Gadu <81575109+Asmblxer@users.noreply.github.com>
2024-05-11 01:31:28 +0300

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,10 +1,10 @@
 
 <?php
 // Database configuration
-$host = "srv1368.hstgr.io"; // Replace with your database host
-$dbName = "u537321905_alandalos"; // Replace with your database name
-$username = "u537321905_alandalos"; // Replace with your database username
-$password = "Alandalos@Furniture123###"; // Replace with your database password
+$host = ""; // Replace with your database host
+$dbName = ""; // Replace with your database name
+$username = ""; // Replace with your database username
+$password = ""; // Replace with your database password
 
 // Create database connection
 $conn = new mysqli($host, $username, $password, $dbName);
